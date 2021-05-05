80a5e4bb3919d540f43d7ec3547d25afc716b1d5
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2021-05-05 09:35:54 +0200

    Feat added necessary items to complete PcscPluginAdapter and PcscReaderAdapter (keyple-cpp-plugin-pcsc)

---
+++ src/main/KeyplePluginExtensionFactory.h
@@ -0,0 +1,34 @@
+/**************************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association                                                *
+ * https://www.calypsonet-asso.org/                                                               *
+ *                                                                                                *
+ * See the NOTICE file(s) distributed with this work for additional information regarding         *
+ * copyright ownership.                                                                           *
+ *                                                                                                *
+ * This program and the accompanying materials are made available under the terms of the Eclipse  *
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0                  *
+ *                                                                                                *
+ * SPDX-License-Identifier: EPL-2.0                                                               *
+ **************************************************************************************************/
+
+#pragma once
+
+/* Commons */
+#include "KeypleCommonsApiExport.h"
+
+namespace keyple {
+namespace core {
+namespace common {
+
+/**
+ * Generic type for a factory of Keyple plugin extension.
+ *
+ * <p>Any Keyple plugin must provide a factory that implements this interface.
+ *
+ * @since 2.0
+ */
+class KEYPLECOMMONSAPI_EXPORT KeyplePluginExtensionFactory {};
+
+}
+}
+}
